ade70e6f10ff68f4c0a6074f090bbe90c9db8024
Dabra56 <david.alexandre.brassard@gmail.com>
2023-01-26 15:24:26 -0500

    Add files via upload

--- february_briefing.R
+++ february_briefing.R
@@ -1,24 +1,28 @@
 library(tidyverse)
 library(cansim)
 library(rvest)
-library(gtrendsR)
 library(openxlsx)
 library(lubridate)
+library(openssl)
+library(httpuv)
+library(rtweet)
+library(twitteR)
+
 
 #Comment
 
-gdp_nowcast_df <-  read.xlsx("https://www.frbatlanta.org/-/media/Documents/cqer/researchcq/gdpnow/GDPTrackingModelDataAndForecasts.xlsx",sheet="ContribHistory",detectDates = TRUE,cols = (2:50))
+gdp_nowcast_US_df <-  read.xlsx("https://www.frbatlanta.org/-/media/Documents/cqer/researchcq/gdpnow/GDPTrackingModelDataAndForecasts.xlsx",sheet="ContribHistory",detectDates = TRUE,cols = (2:50))
 
 
-gdp_nowcast <- 
-  gdp_nowcast_df %>% 
+gdp_nowcast_US <- 
+  gdp_nowcast_US_df %>% 
     filter(X1 == "GDP Nowcast")
 
 #date_vector <- colnames(gdp_nowcast_df)
 
 #gdp_nowcast_df <- gdp_nowcast_df %>% mutate(Date=date_vector)
 
-write.csv(x = gdp_nowcast, file="gdp_nowcast.csv")
+write.csv(x = gdp_nowcast_US, file="gdp_nowcast.csv")
 
 cfnai_df <- read.xlsx("https://www.chicagofed.org/-/media/publications/cfnai/cfnai-data-series-xlsx.xlsx", sheet="data",
                       detectDates = TRUE)
@@ -31,6 +35,16 @@ cfnai <-
 
 write.csv(x = cfnai, file="cfnai.csv")
 
+# US 
+
+wei<-  read.xlsx("https://www.newyorkfed.org/medialibrary/research/interactives/wei/downloads/weekly-economic-index_data.xlsx",sheet="WEI for Recent Months",detectDates = TRUE, rows = (5:50))
+
+wei <- 
+  wei %>% 
+    select(Date,WEI)
+
+write.csv(x = wei, file="wei.csv")
+
 # Getting weekly Employment insurance data 
 ei_monthly <- get_cansim_vector("v64549350") %>% 
       filter(Date> "2018-12-01") %>% 
@@ -40,7 +54,7 @@ colnames(ei_monthly)[2] <- "Number of beneficiairies"
   
 
 # Creating starting file because weekly data is upgraded with no trace of historical data
-ei_file <- read.csv("data/employment_insurance.csv")
+ei_file <- read.csv("employment_insurance.csv")
 
 ei_file <- ei_file [2:4]
 
@@ -67,7 +81,7 @@ Date <- word(ei_week,start=4,-1)
 
 ei_table <- tibble(Date,ei_regular_beneficiairies,ei_newclaim)
 
-if (dmy(ei_file[1,1])!=mdy(Date)) {
+if (mdy(ei_file[1,1])!=mdy(Date)) {
   
   ei_file <- 
     ei_file %>% bind_rows(ei_table)
@@ -75,16 +89,16 @@ if (dmy(ei_file[1,1])!=mdy(Date)) {
   print("No new data")
 }
 
-ei_file <- 
-  ei_file %>% 
-      mutate(Date= as.Date(dmy(Date)))
+# ei_file <- 
+#   ei_file %>% 
+#       mutate(Date= as.Date(dmy(Date)))
 
 ei_datawrapper <- 
   ei_monthly %>% 
-  bind_rows(ei_file) %>%
-    mutate(`Number of beneficiairies` = case_when(is.na(`Number of beneficiairies`)~ "R" ))
+  bind_rows(ei_file %>% 
+              mutate(Date= as.Date(mdy(Date))))
 
-write.csv(x = ei_datawrapper, file="ei_data.csv")
+write.table(ei_datawrapper, file = "employment_insurance_final.csv") 
 
 business_conditions <-get_cansim("33-10-0398-01")
 
@@ -163,63 +177,99 @@ colnames(business_conditions_city)[5] <-  "Variations since previous year ^in %^
 write.csv(x = business_conditions_city, file="business_condition.csv")
 
 # GOOGLE TRENDS 
+# 
+# install.packages("gtrendsR")
+ library(gtrendsR)
+ 
+  # search_terms <- c("Recession", 
+  #                   "Unemployment",
+  #                   "Inflation")
 
+gtrends(keyword = "Recession",
+         geo = "CA",
+         time = "today 12-m") -> recession_results
 
-search_terms <- c("Recession", 
-                  "Unemployment",
-                  "Inflation")
+gtrends_recession <- recession_results %>%
+  .$interest_over_time %>%
+  select(date,hits) %>%
+  rename(Recession = hits)
 
-output_results <- gtrends(keyword = search_terms,
+Sys.sleep(100)
+
+gtrends(keyword = "Unemployment",
         geo = "CA",
-        time = "today 12-m") 
+        time = "today 12-m") -> unemployment_results
 
-gtrends <- output_results %>%
-  .$interest_over_time
+gtrends_unemployment <- unemployment_results %>%
+  .$interest_over_time %>%
+  select(date,hits) %>%
+  rename(Unemployment = hits)
 
-gtrends_datawrapper <- 
-gtrends %>%
-  pivot_wider( names_from = keyword,
-                values_from = hits ) %>% 
-  select(date,Recession,Unemployment,Inflation)
+Sys.sleep(100)  
 
-gtrends_recession <- 
-  gtrends_datawrapper %>% 
-      select(date, Recession)
+gtrends(keyword = "Inflation",
+        geo = "CA",
+        time = "today 12-m") -> inflation_results
 
-gtrends_unemployment<- 
-  gtrends_datawrapper %>% 
-  select(date, Unemployment)
-
-gtrends_inflation<- 
-  gtrends_datawrapper %>% 
-  select(date, Inflation)
+gtrends_inflation <- inflation_results %>%
+  .$interest_over_time %>%
+  select(date,hits) %>%
+  rename(Inflation = hits)
 
 write.csv(x = gtrends_recession, file="gtrend_recession.csv")
 write.csv(x = gtrends_unemployment, file="gtrend_unemployment.csv")
 write.csv(x = gtrends_inflation, file="gtrends_inflation.csv")
-  
-  # #Twitter 
-# library("openssl")
-# library("httpuv")
-# library(rtweet)
-# library(twitteR)
-# 
-# get_token()
-# 
-# api_key <- "RImdmIWcLymaoMpNz4FRYuFHn"
-# api_secret_key <- "YODJ9F1vz1PFHzd5ORPoSHkWOwtCpoLLF2yJ1sLf7o1tOmJgPZ"
-# 
-# #Beared Token = "AAAAAAAAAAAAAAAAAAAAAEGwlQEAAAAA1KVzcV5965DQBJt%2BIGf%2FhRXNENo%3DpPtKvZvUkrWMMJZ7Egb8uB2NYcyqDLNptSyCtG1sQDWXabXh1h"
-# 
-# access_token <- "1218979300701102081-M9aFzFH5DVtj25VndfW12xcslQ6Gs0"
-# 
-# access_secret  <- "mKqGjEwFNLv8NmqcYDuW2HDZJbPE7Kw25KixMQwOEP3F0"
-# 
-# setup_twitter_oauth(api_key,
-#                     api_secret_key,
-#                     access_token,
-#                     access_secret )
-# 
+
+#Twitter
+
+nowcast_gdp_file <- read.csv("nowcast_gdp.csv")
+
+
+appname <- "Nowcast_CPACanada"
+api_key <- "RImdmIWcLymaoMpNz4FRYuFHn"
+api_secret_key <- "YODJ9F1vz1PFHzd5ORPoSHkWOwtCpoLLF2yJ1sLf7o1tOmJgPZ"
+
+#Beared Token = "AAAAAAAAAAAAAAAAAAAAAEGwlQEAAAAA1KVzcV5965DQBJt%2BIGf%2FhRXNENo%3DpPtKvZvUkrWMMJZ7Egb8uB2NYcyqDLNptSyCtG1sQDWXabXh1h"
+
+access_token <- "1218979300701102081-M9aFzFH5DVtj25VndfW12xcslQ6Gs0"
+
+access_token_secret  <- "mKqGjEwFNLv8NmqcYDuW2HDZJbPE7Kw25KixMQwOEP3F0"
+
+twitter_token <- create_token(
+  app=appname, 
+  consumer_key = api_key, 
+  consumer_secret = api_secret_key, 
+  access_token = access_token, 
+  access_secret = access_token_secret
+)
+
+get_token()
+
+tweet_nowcast_canada <- search_tweets("@IFSD_IFPD", n = 100, include_rts = FALSE)
+
+nowcast_gdp <- 
+  tweet_nowcast_canada %>% 
+  mutate (nowcast_gdp =str_extract(text, "(\\d\\.\\d%)\\s\\(q\\/q, annualized\\)"),
+          created_at = as.Date(created_at)) %>% 
+  select(created_at,nowcast_gdp) %>% 
+  rename(date= created_at) %>% 
+  arrange(desc(date)) %>% 
+  drop_na(nowcast_gdp) %>% 
+  mutate(nowcast_gdp=substr(nowcast_gdp,1,4))
+
+
+nowcast_gdp_file <- 
+  nowcast_gdp_file %>% 
+      select(-X) %>% 
+      mutate(date=as.Date(date)) %>% 
+      bind_rows(nowcast_gdp) %>% 
+      arrange(date)
+
+nowcast_gdp_file <- nowcast_gdp_file[!duplicated(nowcast_gdp_file), ]
+
+write.csv(nowcast_gdp_file, file = "nowcast_gdp.csv", append = TRUE) 
+
+
 # searchTwitter("Covid-19", n=3)
 # 
 # #APP ID = 26587201
